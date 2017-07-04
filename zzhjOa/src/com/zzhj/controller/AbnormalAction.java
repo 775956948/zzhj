a7999df105d4ab3ecaa459447b4af4cc84c1ed5d
775956948 <775956948@qq.com>
2017-07-04 13:36:49 +0800

    07-04

--- zzhjOa/src/com/zzhj/controller/AbnormalAction.java
+++ zzhjOa/src/com/zzhj/controller/AbnormalAction.java
@@ -1,21 +1,26 @@
 package com.zzhj.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.zzhj.po.Abnormal;
-import com.zzhj.po.Rest;
+
 import com.zzhj.po.Users;
 import com.zzhj.service.AbnormalService;
-import com.zzhj.service.RestService;
+
 
 @Controller
 @RequestMapping("/abnormal")
@@ -46,5 +51,6 @@ public class AbnormalAction {
 	public int deleteRest(int id){
 		return as.deleteRest(id);
 	}
+	
 	
 }
